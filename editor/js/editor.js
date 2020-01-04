d1d6d1eb52498e385ca01f5f604389ac0579042c
Léon Lenclos <leon.lenclos@gmail.com>
2020-01-04 13:50:59 +0100

    editeur

--- editor/js/editor.js
+++ editor/js/editor.js
@@ -3,6 +3,20 @@ $.ajaxSetup({cache: false});
 $(document).ready(main);
 
 function main(argument) {
+        let async = $.ajax({
+            url:"editor/js/special.json",
+            dataType: "json",
+            async: true,
+
+        })
+        .done((data)=>{
+            $('#hello-world').html(data.txt)
+        })
+        .fail((jqXHR, textStatus, errorThrown)=>{
+            $('#hello-world').html('...')
+                
+        });
+
     $("#auto-assets").click(()=>{
         console.log('clic')
       $.ajax({
@@ -17,5 +31,31 @@ function main(argument) {
                 alert('Erreur!');
             }
             });
+    })
+
+
+    $("#update-hello-world").click(()=>{
+        console.log('clic')
+    let helloworld= prompt('', $('#hello-world').html())
+
+            $.ajax({
+            type: "POST",
+            url: "/set_json",
+            data: JSON.stringify({
+                path:"editor/js/special.json",
+                data:{txt:helloworld}
+            })  ,
+            // contentType: "application/json; charset=utf-8",
+            // dataType: "json",
+            success: (data)=>{
+                $('#hello-world').html(helloworld)
+                alert('cool.');},
+            error: function(errMsg) {
+                console.log('error!', errMsg)
+                alert('Error!   ');
+            }
+            });
+
+
     })
 }
